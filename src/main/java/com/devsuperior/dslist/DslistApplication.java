7813245d30e01c5a7b3eb4c1c40fc7000ddf3c79
SosthenesMS <sosthenesms@gmail.com>
2023-07-16 18:14:15 -0300

    Entity, Service, Controller e DTO

--- src/main/java/com/devsuperior/dslist/DslistApplication.java
+++ src/main/java/com/devsuperior/dslist/DslistApplication.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DslistApplication {
 
@@ -10,4 +11,5 @@ public class DslistApplication {
 		SpringApplication.run(DslistApplication.class, args);
 	}
 
+			
 }
